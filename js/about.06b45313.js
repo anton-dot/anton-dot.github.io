78e3dac6eefd7f98a85aeaa4a2cfc1d087d563b3
Anton <anton.kurakin@platform-s.com>
2023-10-15 18:12:32 +0300

    init

---
+++ js/about.06b45313.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkmenglish"]=self["webpackChunkmenglish"]||[]).push([[443],{4007:function(n,e,s){s.r(e),s.d(e,{default:function(){return o}});var u=s(3396);const t={class:"about"},a=(0,u._)("h1",null,"This is an about page",-1),r=[a];function i(n,e){return(0,u.wg)(),(0,u.iD)("div",t,r)}var c=s(89);const l={},h=(0,c.Z)(l,[["render",i]]);var o=h}}]);
+//# sourceMappingURL=about.06b45313.js.map
